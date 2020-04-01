526e70f81ad07a0fce19e4d5fdbc1da66e9f751b
blessednut <bram.verheijen90@gmail.com>
2020-04-02 00:35:28 +0200

    test
    test

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -3,6 +3,7 @@ package Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -15,7 +16,9 @@ public class Main extends Application {
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("SAGRADA");
 		Scene scene = new Scene(new Pane());
+		scene.setFill(Color.AQUA);
 		stage.setScene(scene);
+		stage.setFullScreen(true);
 		stage.show();
 
 	}
