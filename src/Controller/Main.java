11d094fcc009b56f4e5f0924376d7f03de9f8c64
blessednut <bram.verheijen90@gmail.com>
2020-04-02 00:37:23 +0200

    eureka
    facking finally

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("SAGRADA");
 		Scene scene = new Scene(new Pane());
-		scene.setFill(Color.AQUA);
+		scene.setFill(Color.AQUAMARINE);
 		stage.setScene(scene);
 		stage.setFullScreen(true);
 		stage.show();
