c21ae70aad708518784435a450bf6ddcd4da57d0
blessednut <bram.verheijen90@gmail.com>
2020-04-23 15:57:20 +0200

    mainclass right

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,18 +1,11 @@
 package Controller;
 
-import View.DiceView;
-import View.FieldView;
-import View.WindowPatternView;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -29,9 +22,8 @@ public class Main extends Application {
 //		stage.setAlwaysOnTop(true);
 		stage.setScene(myscene.getMyscene());
 		stage.show();
+		myscene.getMyscene().addEventHandler(KeyEvent.KEY_PRESSED, new MyKeyHandler());
 	}
-
-	myscene.getMyscene().addEventHandler(KeyEvent.KEY_PRESSED, new MyKeyHandler());
 
 	private class MyKeyHandler implements EventHandler<KeyEvent> {
 
@@ -44,7 +36,4 @@ public class Main extends Application {
 
 	}
 
-	private int numberGen(int min, int max) {
-		return (int) ((Math.random() * ((max - min) + 1)) + min);
-	}
 }
