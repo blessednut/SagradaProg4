42cfb8070406facfd03d01c476d8f96023f68cc3
blessednut <bram.verheijen90@gmail.com>
2020-04-23 08:59:12 +0200

    invitepan aangepast en stats aangepast

--- src/View/InvitePane.java
+++ src/View/InvitePane.java
@@ -1,94 +1,119 @@
 package View;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Screen;
 
-public class InvitePane extends HBox{
-	
+public class InvitePane extends HBox {
+
 	private double screenX = Screen.getPrimary().getVisualBounds().getWidth();
 	private double screenY = Screen.getPrimary().getVisualBounds().getHeight();
-	
+
 	private VBox vbox;
 	private VBox vbox2;
-	
-	private Text titel1;
+
+	private Label titel1;
+	private Label titel2;
+	private TextArea Username;
 	private TextField name;
 	private Button search;
 	private Button inviteButton;
-	
-	private Text titel2;
+
+//	private Text titel2;
 	private Text name2;
 	private Button accept;
 	private Button ignore;
 	private HBox hbox2;
-	
-	
+
+	private ImageView labelBackground = new ImageView(new Image("Resources/Label_uitnodigen.png"));
+	private ImageView labelNameBackground = new ImageView(new Image("Resources/Label_uitnodigen.png"));
+
 	public InvitePane() {
-		this.setMinSize((screenX-200), screenY);
-		this.setMaxSize((screenX-200), screenY); 
-		this.setPrefSize((screenX-200), screenY);
+		this.setMinSize((screenX - 200), screenY);
+		this.setMaxSize((screenX - 200), screenY);
+		this.setPrefSize((screenX - 200), screenY);
 		createPane();
 		showInvite("piet");
 	}
-	
+
 	private void createPane() {
-		titel1 = new Text("vrienden uitnodigen");
-		
+		labelBackground.setFitWidth(300);
+		labelBackground.setFitHeight(100);
+		titel1 = new Label("vrienden uitnodigen", labelBackground);
+		titel1.setFont(new Font("Arial", 20));
+		titel1.setStyle("-fx-font-weight: bold");
+		titel1.setContentDisplay(ContentDisplay.CENTER);
+//		titel1.setOpacity(0.4);
+
 		name = new TextField();
 		name.setText("Gebruikersnaam");
+		name.setOnMouseClicked(e -> name.clear());
 		name.setMinSize(300, 100);
 		name.setPrefSize(300, 100);
 		name.setMaxSize(300, 100);
-		
+
 		search = new Button();
 		search.setText("zoek");
-		search.setMinSize(300, 100);
-		search.setPrefSize(300, 100);
-		search.setMaxSize(300, 100);
-		
+		search.setMinSize(150, 30);
+		search.setPrefSize(150, 30);
+		search.setMaxSize(150, 30);
+
 		inviteButton = new Button();
 		inviteButton.setText("uitnodigen");
-		inviteButton.setMinSize(300, 100);
-		inviteButton.setPrefSize(300, 100);
-		inviteButton.setMaxSize(300, 100);
+		inviteButton.setMinSize(15, 30);
+		inviteButton.setPrefSize(150, 30);
+		inviteButton.setMaxSize(150, 30);
 		
-		titel2 = new Text("openstaande uitnodigingen");
-		
-		vbox = new VBox(titel1, name, search, inviteButton);
-		vbox2 = new VBox(titel2);
+		HBox hbox = new HBox(search, inviteButton);
+		vbox = new VBox(titel1, name, hbox);
+		vbox2 = new VBox();
 		this.getChildren().addAll(vbox, vbox2);
 	}
-	
+
 	public void showInvite(String username) {
 		hbox2 = new HBox();
-		
-		name2 = new Text();
-		name2.setText(username);
-		
+
+		labelNameBackground.setFitWidth(300);
+		labelNameBackground.setFitHeight(100);
+
+		titel2 = new Label("openstaande uitnodigingen", labelNameBackground);
+		titel2.setFont(new Font("Arial", 20));
+		titel2.setStyle("-fx-font-weight: bold");
+		titel2.setContentDisplay(ContentDisplay.CENTER);
+
+		Username = new TextArea();
+		Username.setMinSize(300, 100);
+		Username.setPrefSize(300, 100);
+		Username.setMaxSize(300, 100);
+		Username.setText(username);
+
 		accept = new Button();
 		accept.setText("accepteer");
-		accept.setMinSize(100, 100);
-		accept.setPrefSize(100, 100);
-		accept.setMaxSize(100, 100);
-		
-		ignore= new Button();
+		accept.setMinSize(150, 30);
+		accept.setPrefSize(150, 30);
+		accept.setMaxSize(150, 30);
+
+		ignore = new Button();
 		ignore.setText("negeer");
-		ignore.setMinSize(100, 100);
-		ignore.setPrefSize(100, 100);
-		ignore.setMaxSize(100, 100);
-		
-		hbox2.getChildren().addAll(name2, accept, ignore);
-		vbox2.getChildren().add(hbox2);
+		ignore.setMinSize(150, 30);
+		ignore.setPrefSize(150, 30);
+		ignore.setMaxSize(150, 30);
+
+		hbox2.getChildren().addAll(accept, ignore);
+		vbox2.getChildren().addAll(titel2, Username, hbox2);
 	}
-	
-	public void showPane(){
-		
+
+	public void showPane() {
+
 	}
-	
+
 }
--- src/View/StatisticsPane.java
+++ src/View/StatisticsPane.java
@@ -1,11 +1,15 @@
 package View;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 import javafx.stage.Screen;
 
 public class StatisticsPane extends HBox{
@@ -15,38 +19,60 @@ public class StatisticsPane extends HBox{
 	
 	private VBox vbox;
 	private VBox vbox2;
-	
-	private Text titel1;
-	
-	private Text titel2;
+		
 	private TextField naam;
 	private Button zoek;
+	
+	private ImageView labelBackground = new ImageView(new Image("Resources/Label_uitnodigen.png"));
+	private ImageView labelStatsBackground = new ImageView(new Image("Resources/Label_uitnodigen.png"));
+	
+	private Label titel1;
+	private Label titel2;
+	private TextArea rangText;
 	
 	public StatisticsPane() {
 		this.setMinSize((screenX-200), screenY);
 		this.setMaxSize((screenX-200), screenY); 
 		this.setPrefSize((screenX-200), screenY);
 		createPane();
+		naam.setOnMouseClicked(e -> naam.clear());
 	}
 	
 	private void createPane() {
-		titel1 = new Text("ranglijst");
 		
-		titel2 = new Text("statestieken");
+		labelBackground.setFitWidth(150);
+		labelBackground.setFitHeight(100);
+		titel1 = new Label("ranglijst", labelBackground);
+		titel1.setFont(new Font("Arial", 20));
+		titel1.setStyle("-fx-font-weight: bold");
+		titel1.setContentDisplay(ContentDisplay.CENTER);
+//		
+		rangText = new TextArea();
+		rangText.setMinSize(150, 70);
+		rangText.setPrefSize(150, 70);
+		rangText.setMaxSize(150, 70);
+		
+		labelStatsBackground.setFitWidth(150);
+		labelStatsBackground.setFitHeight(100);
+
+		titel2 = new Label("statestieken", labelStatsBackground);
+		titel2.setFont(new Font("Arial", 20));
+		titel2.setStyle("-fx-font-weight: bold");
+		titel2.setContentDisplay(ContentDisplay.CENTER);
 		
 		naam = new TextField();
 		naam.setText("Gebruikersnaam");
-		naam.setMinSize(300, 100);
-		naam.setPrefSize(300, 100);
-		naam.setMaxSize(300, 100);
+		naam.setMinSize(150, 70);
+		naam.setPrefSize(150, 70);
+		naam.setMaxSize(150, 70);
 		
 		zoek = new Button();
 		zoek.setText("zoek");
-		zoek.setMinSize(300, 100);
-		zoek.setPrefSize(300, 100);
-		zoek.setMaxSize(300, 100);
+		zoek.setMinSize(150, 70);
+		zoek.setPrefSize(150, 70);
+		zoek.setMaxSize(150, 70);
 		
-		vbox = new VBox(titel1);
+		vbox = new VBox(titel1, rangText);
 		vbox2 = new VBox(titel2, naam, zoek);
 		this.getChildren().addAll(vbox, vbox2);
 	}
