446ebd3db5d2f730e9a674e7dd516b2d1df55260
Brum <47247246+BrumBrummert@users.noreply.github.com>
2020-04-28 19:52:57 +0200

    FieldView verwijderd
    FieldView is nu vervangen door squareView.
    De code om een highlight te geven over het vak waar de muis overheen hoverd is toegevoegd aan de squareView en uitgecomment.

--- src/View/squareView.java
+++ src/View/squareView.java
@@ -4,6 +4,7 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
@@ -38,4 +39,26 @@ public class squareView extends Pane {
 		BackgroundFill fill = new BackgroundFill(converter.colorConverter(colorProperty.getValue()), null, null);
 		return new Background(fill);
 	}
+	
+//	this.setOnMouseEntered(event -> fieldHover());
+//	this.setOnMouseExited(event -> fieldUnhover());
+//	
+//	private void fieldHover() {
+//		DropShadow shadow1 = new DropShadow();
+//		shadow1.setColor(Color.GREEN);
+//		shadow1.setOffsetX(2);
+//		shadow1.setOffsetY(2);
+//
+//		DropShadow shadow2 = new DropShadow();
+//		shadow2.setColor(Color.GREEN);
+//		shadow2.setOffsetX(-2);
+//		shadow2.setOffsetY(-2);
+//
+//		shadow2.setInput(shadow1);
+//		this.setEffect(shadow2);
+//	}
+//
+//	private void fieldUnhover() {
+//		this.setEffect(null);
+//	}
 }
