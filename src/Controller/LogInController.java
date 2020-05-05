5d773950da998dc809596be000b27d0dafa16477
Ephemerial <38559102+Ephemerial@users.noreply.github.com>
2020-05-05 23:18:28 +0200

    Revert "reverse changes"
    This reverts commit d7dc5d5a751436674f6902259b21ba6d92333593.

--- src/Controller/LogInController.java
+++ src/Controller/LogInController.java
@@ -11,31 +11,44 @@ public class LogInController {
 	private LoginPane v_login;
 	private LoginModel m_login;
 	private MySceneController c_myscene;
-
+<<<<<<< HEAD
+<<<<<<< HEAD
 	private String username;
 	private HomeThreadController c_hometc;
 
-//	private String username;
-
+	private String username;
+=======
+>>>>>>> parent of 5e7dfa1... fixed the exception for the invite thread
+=======
+>>>>>>> parent of 5e7dfa1... fixed the exception for the invite thread
 
 	public LogInController(MySceneController c_myscene) {
 		v_login = new LoginPane();
 		m_login = new LoginModel();
 		this.c_myscene = c_myscene;
+<<<<<<< HEAD
+<<<<<<< HEAD
+		v_login.getLogin().setOnAction(e -> SetInlogInfo());
+		v_login.getRegister().setOnAction(e -> m_login.getDbcon().registerLogin(v_login.getUsername().getText(),
+				v_login.getPassword().getText()));
+=======
+		GamePane game = new GamePane();
+>>>>>>> parent of 5e7dfa1... fixed the exception for the invite thread
 
 		v_login.getLogin().setOnAction(e -> SetInlogInfo());
 		v_login.getRegister().setOnAction(e -> m_login.getDbcon().registerLogin(v_login.getUsername().getText(),
 				v_login.getPassword().getText()));
+<<<<<<< HEAD
+
+=======
+		GamePane game = new GamePane();
 
 		v_login.getLogin().setOnAction(e -> SetInlogInfo());
 		v_login.getRegister().setOnAction(e -> m_login.getDbcon().registerLogin(v_login.getUsername().getText(),
 				v_login.getPassword().getText()));
-//		GamePane game = new GamePane();
-
-		v_login.getLogin().setOnAction(e -> SetInlogInfo());
-		v_login.getRegister().setOnAction(e -> m_login.getDbcon().registerLogin(v_login.getUsername().getText(),
-				v_login.getPassword().getText()));
-
+>>>>>>> parent of 5e7dfa1... fixed the exception for the invite thread
+=======
+>>>>>>> parent of 5e7dfa1... fixed the exception for the invite thread
 		v_login.addEventHandler(KeyEvent.KEY_PRESSED, new MyEnterHandler());
 	}
 
@@ -74,10 +87,17 @@ public class LogInController {
 		return username;
 
 	}
-
+<<<<<<< HEAD
+<<<<<<< HEAD
 
 	public String getUsername() {
 		return username;
 	}
+	
+	
+=======
+>>>>>>> parent of 5e7dfa1... fixed the exception for the invite thread
+=======
+>>>>>>> parent of 5e7dfa1... fixed the exception for the invite thread
 
 }
--- src/Controller/MySceneController.java
+++ src/Controller/MySceneController.java
@@ -10,13 +10,17 @@ public class MySceneController {
 
 	public MySceneController() {
 		v_scene = new MyScene(this);
-
+<<<<<<< HEAD
+<<<<<<< HEAD
 		c_home = new HomeController(this, c_login);
 		System.out.println("hoi");
 
-//		c_home = new HomeController(this);
-
-
+=======
+		c_home = new HomeController(this);
+>>>>>>> parent of 5e7dfa1... fixed the exception for the invite thread
+=======
+		c_home = new HomeController(this);
+>>>>>>> parent of 5e7dfa1... fixed the exception for the invite thread
 	}
 
 	public MyScene getMyscene() {
