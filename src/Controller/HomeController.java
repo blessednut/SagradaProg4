42187efd03be05e533cf0c06848c92730c3e2268
blessednut <bram.verheijen90@gmail.com>
2020-05-05 18:13:21 +0200

    fixed

--- src/Controller/HomeController.java
+++ src/Controller/HomeController.java
@@ -1,25 +1,26 @@
 package Controller;
 
 import View.CreditsPane;
-import View.GamePane;
 import View.HomePane;
 import View.InvitePane;
 import View.StatisticsPane;
+import model.HomeThreadModel;
 
 public class HomeController {
 
 	private HomePane v_home;
+
 	private InvitePane v_invite;
+
+	private InviteController c_Invite;
+
 	private CreditsPane v_credits;
 	private StatisticsPane v_statistics;
 	private MySceneController myScene;
 	private GameController c_game;
-<<<<<<< Updated upstream
-=======
 	private LogInController c_login;
 	private HomeThreadModel m_home;
 	private HomeThreadController c_hometc;
->>>>>>> Stashed changes
 
 	public HomeController(MySceneController myScene) {
 		this.myScene = myScene;
@@ -35,7 +36,7 @@ public class HomeController {
 		v_home.getCredits().setOnAction(e -> openCreditsPane());
 		v_home.getGames().setOnAction(e -> c_game.setGamePane());
 	}
-	
+
 	public void openInvitePane() {
 		v_home.makeReservedSpace(v_invite);
 	}
@@ -43,7 +44,7 @@ public class HomeController {
 	public void openStatisticsPane() {
 		v_home.makeReservedSpace(v_statistics);
 	}
-	
+
 	public void openCreditsPane() {
 		v_home.makeReservedSpace(v_credits);
 	}
@@ -59,13 +60,11 @@ public class HomeController {
 	public StatisticsPane getV_statistics() {
 		return v_statistics;
 	}
-<<<<<<< Updated upstream
-=======
+
 	private void threadMethod() throws NullPointerException {
 		System.out.println("Test");
 		Thread th = new Thread(c_hometc);
 		th.start();
 	}
->>>>>>> Stashed changes
 
 }
--- src/Controller/LogInController.java
+++ src/Controller/LogInController.java
@@ -14,27 +14,27 @@ public class LogInController {
 	private LoginModel m_login;
 
 	private MySceneController c_myscene;
-<<<<<<< Updated upstream
-=======
+
 	private String username;
-	
->>>>>>> Stashed changes
 
 	public LogInController(MySceneController c_myscene) {
 		v_login = new LoginPane();
 		m_login = new LoginModel();
 		this.c_myscene = c_myscene;
-<<<<<<< Updated upstream
+
 		GamePane game = new GamePane();
+
+		v_login.getLogin().setOnAction(e -> SetInlogInfo());
+
+		v_login.getLogin().setOnAction(e -> SetInlogInfo());
+
+		v_login.getRegister().setOnAction(e -> m_login.getDbcon().registerLogin(v_login.getUsername().getText(),
+				v_login.getPassword().getText()));
 
 		v_login.getLogin().setOnAction(e -> SetInlogInfo());
 		v_login.getRegister().setOnAction(e -> m_login.getDbcon().registerLogin(v_login.getUsername().getText(),
 				v_login.getPassword().getText()));
-=======
 
-		v_login.getLogin().setOnAction(e -> SetInlogInfo());
-		v_login.getRegister().setOnAction(e -> m_login.getDbcon().registerLogin(v_login.getUsername().getText(), v_login.getPassword().getText()));
->>>>>>> Stashed changes
 		v_login.addEventHandler(KeyEvent.KEY_PRESSED, new MyEnterHandler());
 	}
 
@@ -72,13 +72,5 @@ public class LogInController {
 		}
 
 	}
-<<<<<<< Updated upstream
-=======
-	
-	
-	
-	
-	
->>>>>>> Stashed changes
 
 }
